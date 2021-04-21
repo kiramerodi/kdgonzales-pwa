dbfe4a78a3d202c4ad725601df717ff89b8c4b1a
kiramerodi <80587402+kiramerodi@users.noreply.github.com>
2021-04-21 10:33:40 +0800

    Updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,14 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticPWA = "cmsc207-pwa";
 const assets = [
   "/",
   "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/styles.css",
+  "/js/main.js",
+  "/images/about.jpg",
+  "/images/home.jpg",
+  "/images/plate1.jpg",
+  "/images/plate2.jpg",
+  "/images/plate3.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
